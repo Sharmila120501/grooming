b61867ab673bae5d27e6bdcad251670df10cd9e9
Sharmila120501 <sharmilaanandhan1201@gmail.com>
2025-05-23 11:24:31 +0530

    task completed

---
+++ script.js
@@ -0,0 +1,35 @@
+//JS:
+//js is high level programming language which is used to create a interactive web pages.
+//v8 engine
+//char:
+// //client-side scripting lang:
+//2. dynamically type lang
+//3. weakly type lang
+//4. interpreted lang
+//5. synchronus in nature(one task at a time)
+let a = 1
+let b = '2'
+console.log(a - b)
+
+//heap area
+//memory execution phase
+//code execution phase
+
+//var
+// var name; //declaration
+// var name='john'; //initialization
+// name='rohan';//reinitialization/updation
+// var name="rama";//redeclaration
+//let
+// let name; //only declaration
+// let name='sharmi'; //initialization and declaration
+//name="rama";//reinitialization
+//let name='raju'; //! not allowed redeclaration
+
+//const
+// const name;//! not possible
+// const name = 'sharmi'
+// name = 'dih'
+// const name='div';
+
+console.log(true + false)
